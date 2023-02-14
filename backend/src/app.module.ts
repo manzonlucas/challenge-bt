ecc43419b0ebe8870ce024bcfabb5a854f627e8c
manzonlucas <manzonlucasm@gmail.com>
2023-02-14 13:14:59 -0300

    add TypeOrm and its configuration

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
 import { ProductsModule } from './products/products.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [
+    ProductsModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'boxtracker123',
+      database: 'boxtracker_challenge',
+    }),
+  ],
   controllers: [],
   providers: [],
 })
