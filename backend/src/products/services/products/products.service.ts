97c624354c8804cdb421385a551a5055e4f7810d
manzonlucas <manzonlucasm@gmail.com>
2023-02-14 17:31:54 -0300

    get and post methods working. Pending /sorted endpoint

--- backend/src/products/services/products/products.service.ts
+++ backend/src/products/services/products/products.service.ts
@@ -10,14 +10,8 @@ export class ProductsService {
     @InjectRepository(Product) private productRepository: Repository<Product>,
   ) {}
 
-  private fakeProducts = [
-    { name: 'Product1 test', price: 5 },
-    { name: 'Product2 test', price: 10 },
-    { name: 'Product3 test', price: 15 },
-  ];
-
   fetchProducts() {
-    return this.fakeProducts;
+    return this.productRepository.find();
   }
 
   createProduct(productDetails: CreateProductDto) {
