5e78fd8477b54db23d0b0e91516ffab02b62f46a
manzonlucas <manzonlucasm@gmail.com>
2023-02-17 21:18:32 -0300

    add footer info and styles

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,5 +1,11 @@
 export default function Footer() {
   return (
-    <footer className="">this is footer</footer>
+    <footer className="bg-indigo-50 flex justify-between">
+      <p className="w-1/4 text-center">A project developed by Lucas Matías Manzón</p>
+      <div className="flex justify-around w-1/4">
+        <a href="https://www.linkedin.com/in/manzonlucas/" className="underline px-10 hover:text-indigo-50 hover:bg-sky-500">LinkedIn</a>
+        <a href="https://github.com/manzonlucas" className="underline px-10 hover:text-indigo-50 hover:bg-sky-500">GitHub</a>
+      </div>
+    </footer>
   )
 }
